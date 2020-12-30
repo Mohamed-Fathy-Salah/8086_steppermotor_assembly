c868970c5bd9ed9422c28d02593d01f34f8b2a86
Ahmad Nashaat <66720719+AhmadNashaat0@users.noreply.github.com>
2020-12-30 19:34:39 +0200

    add some definations (#2)

--- code.asm
+++ code.asm
@@ -1,20 +1,42 @@
+
+;Componants' place :
+; ADC (potentiometer)       on port A
+; Direction led             on port B 0
+; Write bit of ADC          on port B 1
+; Stop Switch               on port C 4
+; Rotate Switch             on port C 5
+; Stepper motor (driver)    on port C 0-3
+
 .MODEL SMALL
-.DATA
-    PORTA EQU 00H
-    PORTB EQU 02H
-    PORTC EQU 04H
-    CTRLWORD EQU 06H
-    DELAY DW 0FFFFH
-    STEPS DB 00000011B , 00000110B,00001100B,00001001B  ; full step
-    DIR DB 00H;
-;stop portc4
-;rotate portc5
-    
-.CODE
+
+.DATA                    ; Data segment
+
+    PORTA EQU 00H        ; Addresse of port A
+    PORTB EQU 02H        ; Addresse of port B
+    PORTC EQU 04H        ; Addresse of port C
+    CTRLWORD EQU 06H     ; Addresse of port Control Word
+    DELAY DW 0FFFFH      ; Delay Value that will control the motor speed
+    DIR DB 00H           ; Direction of Stepper Motor (0/1) 
+    STEPS DB 00000011B,  ; Full Step Mode
+             00000110B, 
+             00001100B, 
+             00001001B   
+     
+
+
+.STACK                   ; Stack segment
+    DB 128D DUP(0B)
+
+
+
+.CODE                    ; Code segment
 .STARTUP
-    MOV AL ,10010000B ; mode 0 port a input , b output , c output/input 
+
+;-------Configration of Ports---------
+    MOV AL ,10010000B    ; mode 0 port a input , b output , c output/input 
     OUT CTRLWORD , AL
-;-----------------
+
+;-------Main Loop----------
 MAIN PROC
     CALL GETSPEED
     CALL GETPRESSED
@@ -30,21 +52,28 @@ RUN PROC
 
     CCW: ; anti clock wise
 RUN ENDP 
-;----------------- shahendah
+
+;----------------- shahenda
 STOP PROC
     ;stop the motor without exiting the program
 STOP ENDP
-;-----------------nashaat
-GETSPEED PROC
-    ;claculate delay and set delay
+
+;-----------------  Nashaat
+GETSPEED PROC       ;Get input from potentiometer to claculate and set Delay
+
+
 GETSPEED ENDP
+
 ;----------------- yasser
 GETPRESSED PROC
   ; check if the stop or the rotate button is pressed 
 GETPRESSED ENDP
+
 ;----------------- omar
 SLEEP PROC
     ; delay for DELAY cycles
 SLEEP ENDP
 .EXIT
+
+
 end
